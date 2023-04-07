05215db474677c3af74e9cb9469ee4f3e0840b51
lcg9227 <364304134@qq.com>
2023-04-07 15:13:06 +0800

    update

--- main.js
+++ main.js
@@ -13,15 +13,15 @@ Vue.config.productionTip = false
 Vue.use(VueI18n)
 // 通过选项创建 VueI18n 实例
 const i18n = new VueI18n({
-  locale: lang, // 设置地区
-  messages, // 设置地区信息
+	locale: lang, // 设置地区
+	messages // 设置地区信息
 })
 Vue.use(plugin)
 App.mpType = 'app'
-  const app = new Vue({
-    i18n,
-  store,
-  ...App
+const app = new Vue({
+	i18n,
+	store,
+	...App
 })
 app.use(ElementPlus, { size: 'default' }).$mount()
 // #endif
@@ -30,16 +30,16 @@ app.use(ElementPlus, { size: 'default' }).$mount()
 import { createSSRApp } from 'vue'
 import { createI18n } from 'vue-i18n'
 export function createApp() {
-  const app = createSSRApp(App)
-  const i18n = createI18n({
-  	locale: lang,
-  	messages
-  })
-  app.use(i18n)
-  app.use(plugin)
-  app.use(store)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	const i18n = createI18n({
+		locale: lang,
+		messages
+	})
+	app.use(i18n)
+	app.use(plugin)
+	app.use(store)
+	return {
+		app
+	}
 }
 // #endif
